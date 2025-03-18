51972c85812faaacaf86184f0b2754c5119d5e46
Naif <noobprogrammer150@gmail.com>
2025-03-18 04:36:32 +0300

    First Commit

---
+++ BlindAuction.py
@@ -0,0 +1,29 @@
+from os import system
+import Art
+system("cls")
+Art.Intro()
+bidderName=input("Name of the bidder: ")
+bidderBid=int(input("How much would you bid ?: $"))
+#This is a test!
+isTrue=True
+Bidders={bidderName:bidderBid}
+while isTrue:
+    decision=input("Are there anymore bidders admin? 'Yes' or  'No': ").lower()
+    if decision=="yes":
+        system("cls")
+        Art.Intro()
+        bidderName=input("Name of the bidder: ")
+        bidderBid=int(input("How much would you bid ?: $"))
+        Bidders[bidderName]=bidderBid
+    elif decision=="no":
+        highestBid=0
+        for key in Bidders:
+            if Bidders[key] >highestBid:
+                highestBid=Bidders[key]
+                highestBidderName=key
+        Art.Money()
+        print(f"The highest bidder is {highestBidderName} with a bid of ${highestBid}")
+        isTrue=False
+    else:
+        print("Invalid Input !")
+    
